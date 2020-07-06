61ad149b2248568517d118813e287acc701ca2fe
KentoTakimoto <kento.zedd2@gmail.com>
2020-07-06 19:13:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.text = "ボタン"
+    }
+}
